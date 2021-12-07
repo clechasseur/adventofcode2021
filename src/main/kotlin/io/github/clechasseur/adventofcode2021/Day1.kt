7b8f99c3b1272fb9594b44add78c29e2cbbe8e4b
Charles Lechasseur <charles.lechasseur@ingeno.ca>
2021-12-07 17:19:10 -0500

    Nitpick

--- src/main/kotlin/io/github/clechasseur/adventofcode2021/Day1.kt
+++ src/main/kotlin/io/github/clechasseur/adventofcode2021/Day1.kt
@@ -7,8 +7,8 @@ object Day1 {
 
     fun part1(): Int = input.zipWithNext().count { it.first < it.second }
 
-    fun part2(): Int = input.zipWithNext().zipWithNext {
-            p1, p2 -> SlidingWindow(p1.first, p1.second, p2.second)
+    fun part2(): Int = input.zipWithNext().zipWithNext { p1, p2 ->
+        SlidingWindow(p1.first, p1.second, p2.second)
     }.zipWithNext().count { it.first < it.second }
 
     private data class SlidingWindow(val x: Int, val y: Int, val z: Int) : Comparable<SlidingWindow> {
