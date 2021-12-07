ec9487f593b3a6a9f18389abbaf63e07f53ef1df
Charles Lechasseur <charles.lechasseur@ingeno.ca>
2021-12-06 23:12:25 -0500

    Day 1

--- src/main/kotlin/io/github/clechasseur/adventofcode2021/Day1.kt
+++ src/main/kotlin/io/github/clechasseur/adventofcode2021/Day1.kt
@@ -1,5 +1,17 @@
 package io.github.clechasseur.adventofcode2021
 
+import io.github.clechasseur.adventofcode2021.data.Day1Data
+
 object Day1 {
-    fun part1(): Int = 0
+    private val input = Day1Data.input
+
+    fun part1(): Int = input.zipWithNext().count { it.first < it.second }
+
+    fun part2(): Int = input.zipWithNext().zipWithNext {
+            p1, p2 -> SlidingWindow(p1.first, p1.second, p2.second)
+    }.zipWithNext().count { it.first < it.second }
+
+    private data class SlidingWindow(val x: Int, val y: Int, val z: Int) : Comparable<SlidingWindow> {
+        override fun compareTo(other: SlidingWindow): Int = (x + y + z) - (other.x + other.y + other.z)
+    }
 }
--- src/test/kotlin/io/github/clechasseur/adventofcode2021/AdventOfCode2021.kt
+++ src/test/kotlin/io/github/clechasseur/adventofcode2021/AdventOfCode2021.kt
@@ -7,7 +7,12 @@ class AdventOfCode2021 {
     class Day1Puzzles {
         @Test
         fun `day 1, part 1`() {
-            assertEquals(0, Day1.part1())
+            assertEquals(1529, Day1.part1())
+        }
+
+        @Test
+        fun `day 1, part 2`() {
+            assertEquals(1567, Day1.part2())
         }
     }
 }
