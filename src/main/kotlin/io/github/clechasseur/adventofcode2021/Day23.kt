a26cbbc1777d4597684b13a1c8a07b008c1b7c0f
Charles Lechasseur <shiftingbeard@gmx.com>
2022-01-02 14:02:09 -0500

    Day 23 part 1 (too slow)

--- src/main/kotlin/io/github/clechasseur/adventofcode2021/Day23.kt
+++ src/main/kotlin/io/github/clechasseur/adventofcode2021/Day23.kt
@@ -1,5 +1,8 @@
 package io.github.clechasseur.adventofcode2021
 
+import io.github.clechasseur.adventofcode2021.dij.Dijkstra
+import io.github.clechasseur.adventofcode2021.dij.Graph
+import io.github.clechasseur.adventofcode2021.util.Direction
 import io.github.clechasseur.adventofcode2021.util.Pt
 
 object Day23 {
@@ -11,7 +14,7 @@ object Day23 {
           #########
     """.trimIndent()
 
-    fun part1(): Int = 0
+    fun part1(): Int = lowestCost(data.toState(), 0)
 
     private val energyCost = mapOf(
         'A' to 1,
@@ -20,7 +23,10 @@ object Day23 {
         'D' to 1000,
     )
 
-    private data class Room(val pts: List<Pt>, val owners: Char)
+    private data class Room(val pts: List<Pt>, val owners: Char) {
+        fun legalDestinationFor(amphipod: Char, tiles: Map<Pt, Char>): Boolean =
+            amphipod == owners && pts.all { tiles[it]!! in listOf('.', amphipod) }
+    }
 
     private val rooms = listOf(
         Room(listOf(Pt(3, 2), Pt(3, 3)), 'A'),
@@ -35,4 +41,65 @@ object Day23 {
         Pt(7, 1),
         Pt(9, 1),
     )
+
+    private val hallway = (1..11).map { Pt(it, 1) }
+
+    private data class State(val tiles: Map<Pt, Char>) {
+        val amphipods: Map<Pt, Char>
+            get() = tiles.filterValues { it.isLetter() }
+
+        val solved: Boolean
+            get() = rooms.all { room ->
+                room.pts.all { tiles[it]!! == room.owners }
+            }
+
+        fun legalMove(from: Pt, to: Pt): Boolean = !lava.contains(to) && rooms.all { room ->
+            (!room.pts.contains(to) || room.legalDestinationFor(tiles[from]!!, tiles)) &&
+                    (!room.pts.contains(from) || room.owners != tiles[from]!!)
+        } && (!hallway.contains(from) || !hallway.contains(to))
+
+        fun move(from: Pt, to: Pt): State {
+            val newTiles = tiles.toMutableMap()
+            newTiles[from] = '.'
+            newTiles[to] = tiles[from]!!
+            return State(newTiles)
+        }
+
+        override fun toString(): String {
+            return (0..4).joinToString("\n") { y ->
+                (0..12).joinToString("") { x -> (tiles[Pt(x, y)] ?: ' ').toString() }
+            }
+        }
+    }
+
+    private class StateGraph(val state: State, val cost: Int) : Graph<Pt> {
+        override fun allPassable(): List<Pt> = state.tiles.filterValues { it == '.' }.keys.toList()
+        override fun neighbours(node: Pt): List<Pt> = Direction.displacements.mapNotNull { disp ->
+            if (state.tiles.getOrDefault(node + disp, ' ') == '.') node + disp else null
+        }
+        override fun dist(a: Pt, b: Pt): Long = cost.toLong()
+    }
+
+    private fun String.toState() = State(lines().flatMapIndexed { y, line ->
+        line.mapIndexed { x, c -> Pt(x, y) to c }
+    }.toMap())
+
+    private fun lowestCost(state: State, costSoFar: Int): Int {
+        if (state.solved) {
+            return costSoFar
+        }
+
+        val newStates = state.amphipods.flatMap { (pos, amphipod) ->
+            val (dist, _) = Dijkstra.build(StateGraph(state, energyCost[amphipod]!!), pos)
+            dist.mapNotNull { (to, cost) ->
+                if (cost > 0 && cost != Long.MAX_VALUE && state.legalMove(pos, to)) {
+                    state.move(pos, to) to cost.toInt()
+                } else null
+            }
+        }
+
+        return newStates.minOfOrNull { (newState, cost) ->
+            lowestCost(newState, costSoFar + cost)
+        } ?: Int.MAX_VALUE
+    }
 }
